5aeadc8acf6442ceb2753f665e09bb4bdc1553a8
Muhammed Muflim Subair <mohdmuflih498@gmail.com>
2026-03-02 03:21:59 +0530

    final deploy setup

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "Installing spaCy model..."
 python -m spacy download en_core_web_sm
+
+echo "Running Django setup..."
 python manage.py collectstatic --noinput
 python manage.py migrate
